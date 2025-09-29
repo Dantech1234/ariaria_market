0a2d1ec52ed4bc5b422596b7145314b0f0eff379
Dantech1234 <126795754+Dantech1234@users.noreply.github.com>
2025-09-29 22:47:31 +0100

    Create Cart.jsx

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+      <p>Items added to the cart will appear here.</p>
+    </div>
+  );
+}
+
+export default Cart;
