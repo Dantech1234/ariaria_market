b1b8d136e3c1d77f03f5652b0aae0e406513db40
Dantech1234 <126795754+Dantech1234@users.noreply.github.com>
2025-09-29 22:45:53 +0100

    Create Products.jsx

---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Products() {
+  return (
+    <div>
+      <h1>All Products</h1>
+      <p>List of products will appear here.</p>
+    </div>
+  );
+}
+
+export default Products;
