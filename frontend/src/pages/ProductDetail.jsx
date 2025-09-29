5cfaa330ceb06f374e315e92d94377f1b4b8317d
Dantech1234 <126795754+Dantech1234@users.noreply.github.com>
2025-09-29 22:47:00 +0100

    Create ProductDetail.jsx

---
+++ frontend/src/pages/ProductDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProductDetail() {
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Details of the selected product will appear here.</p>
+    </div>
+  );
+}
+
+export default ProductDetail;
