5cfecdf2932651f455081b6b834f460f0b0b0d55
Dantech1234 <126795754+Dantech1234@users.noreply.github.com>
2025-09-29 22:48:05 +0100

    Create SellerDashboard.jsx

---
+++ frontend/src/pages/SellerDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SellerDashboard() {
+  return (
+    <div>
+      <h1>Seller Dashboard</h1>
+      <p>Sellers can manage their products and orders here.</p>
+    </div>
+  );
+}
+
+export default SellerDashboard;
